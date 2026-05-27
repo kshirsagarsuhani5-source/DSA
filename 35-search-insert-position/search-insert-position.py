6a9708f7795e3754ceaca2e25ad34dcae6adbb26
Suhani Kshirsagar <kshirsagarsuhani5@gmail.com>
2026-05-27 22:44:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (94.56%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -5,16 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        left, right = 0, len(nums) - 1
-        
+        left = 0 
+        right = len(nums) - 1
         while left <= right:
-            
-            mid = (left + right) // 2
-            
+            mid = (left + right) //2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
+            if nums[mid] < target:
                 left = mid + 1
             else:
-                right = mid - 1
+                right = mid -1 
         return left
