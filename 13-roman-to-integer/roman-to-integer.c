b62a6d3155c3bc3de59b92a4e2f5de7cfe4343a9
Suhani Kshirsagar <kshirsagarsuhani5@gmail.com>
2026-07-21 16:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (22.70%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.c
@@ -0,0 +1,26 @@
+int value(char c) {
+    switch (c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
+int romanToInt(char *s) {
+    int result = 0;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (value(s[i]) < value(s[i + 1])) {
+            result -= value(s[i]);
+        } else {
+            result += value(s[i]);
+        }
+    }
+
+    return result;
+}
