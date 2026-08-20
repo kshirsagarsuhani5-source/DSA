90d9bf0af444b7e35400593df2d6f4a522ceb8c1
Suhani Kshirsagar <kshirsagarsuhani5@gmail.com>
2026-08-20 22:05:03 +0530

    Time: 430 ms (40.00%) | Memory: 29.8 MB (0.00%) - LeetSync

---
+++ 3674-count-non-decreasing-subarrays-after-k-operations/count-non-decreasing-subarrays-after-k-operations.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def countNonDecreasingSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        from collections import deque
+        ans = 0
+        cost = 0
+
+        dq = deque()
+
+        j = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            num = nums[i]
+            count = 1
+
+            while dq and dq[-1][0] < num:
+                next_num, next_count = dq.pop()
+
+                count += next_count
+                cost += (num - next_num) * next_count
+
+            dq.append((num, count))
+
+            while cost > k:
+                rightmost_num, rightmost_count = dq.popleft()
+
+                cost -= rightmost_num - nums[j]
+                j -= 1
+
+                if rightmost_count > 1:
+                    dq.appendleft(
+                        (rightmost_num, rightmost_count - 1)
+                    )
+
+            ans += j - i + 1
+
+        return ans
+        
